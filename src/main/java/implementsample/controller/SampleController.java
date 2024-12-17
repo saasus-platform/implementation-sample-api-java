42a4b5c774b46dd5739a2ebb38ea6c6d826583ad
jFujiya <shiho.fujiya@anti-pattern.co.jp>
2024-12-17 16:28:41 +0900

    途中保存

--- src/main/java/implementsample/controller/SampleController.java
+++ src/main/java/implementsample/controller/SampleController.java
@@ -1,24 +1,40 @@
 package implementsample.controller;
 
 import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import saasus.sdk.auth.ApiException;
 import saasus.sdk.auth.api.CredentialApi;
+import saasus.sdk.auth.api.TenantApi;
 import saasus.sdk.auth.api.TenantUserApi;
+import saasus.sdk.auth.api.TenantAttributeApi;
 import saasus.sdk.auth.api.UserInfoApi;
+import saasus.sdk.auth.api.UserAttributeApi;
+import saasus.sdk.pricing.api.PricingPlansApi;
 import saasus.sdk.auth.models.Credentials;
 import saasus.sdk.auth.models.UserInfo;
 import saasus.sdk.auth.models.Users;
 import saasus.sdk.modules.AuthApiClient;
+import saasus.sdk.modules.PricingApiClient;
 import saasus.sdk.modules.Configuration;
+import saasus.sdk.auth.models.TenantAttributes;
+import saasus.sdk.auth.models.TenantDetail;
+import saasus.sdk.auth.models.Attribute;
+import saasus.sdk.auth.models.UserAttributes;
+import saasus.sdk.pricing.models.PricingPlan;
 
 @RestController
 public class SampleController {
@@ -98,6 +114,7 @@ public class SampleController {
             throw e;
         }
 
+        System.out.println(userInfo.toJson());
         return ResponseEntity.ok(userInfo.toJson());
     }
 
@@ -137,5 +154,148 @@ public class SampleController {
         }
 
         return ResponseEntity.ok(users.getUsers());
+    }
+
+    @GetMapping(value = "/tenant_attributes", produces = "application/json")
+    public ResponseEntity<?> getTenantAttributes(HttpSession session, HttpServletRequest request) throws Exception {
+        String tenantId = request.getParameter("tenant_id");
+        AuthApiClient apiClient = new Configuration().getAuthApiClient();
+        apiClient.setReferer(request.getHeader("Referer"));
+
+        UserInfoApi userInfoApi = new UserInfoApi(apiClient);
+        UserInfo userInfo = null;
+        try {
+            userInfo = userInfoApi.getUserInfo(getIDToken(request));
+            System.out.println(userInfo);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling UserInfoApi#getUserInfo");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+        System.out.println(userInfo.getTenants());
+
+        TenantAttributeApi tenantAttributeApi = new TenantAttributeApi(apiClient);
+        TenantAttributes tenantAttributes = null;
+        try {
+            tenantAttributes = tenantAttributeApi.getTenantAttributes();
+            System.out.println(tenantAttributes);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling TenantAttributeApi#getTenantAttributes");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+
+        TenantApi tenantApi = new TenantApi(apiClient);
+        TenantDetail tenantDetail = null;
+        try {
+            tenantDetail = tenantApi.getTenant(tenantId);
+            System.out.println(tenantDetail);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling TenantApi#getTenant");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+
+        Map<String, Map<String, Object>> details = new HashMap<>();
+        for (Attribute attribute : tenantAttributes.getTenantAttributes()) {
+            String attributeName = attribute.getAttributeName();
+            Map<String, Object> detail = new HashMap<>();
+            detail.put("display_name", attribute.getDisplayName());
+            detail.put("attribute_type", attribute.getAttributeType());
+            detail.put("value", tenantDetail.getAttributes().get(attributeName));
+
+            details.put(attributeName, detail);
+        }
+        System.out.println(details);
+
+        return ResponseEntity.ok(details);
+    }
+
+    @GetMapping(value = "/user_attributes", produces = "application/json")
+    public ResponseEntity<?> getUserAttributes(HttpSession session, HttpServletRequest request) throws Exception {
+        AuthApiClient apiClient = new Configuration().getAuthApiClient();
+        apiClient.setReferer(request.getHeader("Referer"));
+
+        UserAttributeApi userAttributeApi = new UserAttributeApi(apiClient);
+        UserAttributes userAttributes = null;
+        try {
+            userAttributes = userAttributeApi.getUserAttributes();
+            System.out.println(userAttributes);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling UserAttributeApi#getUserAttributes");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+
+        return ResponseEntity.ok(userAttributes);
+    }
+
+    @GetMapping(value = "/pricing_plan", produces = "application/json")
+    public ResponseEntity<?> getPricingPlan(HttpSession session, HttpServletRequest request, @RequestParam(value = "plan_id", required = false) String planId) throws Exception {
+        AuthApiClient apiClient = new Configuration().getAuthApiClient();
+        apiClient.setReferer(request.getHeader("Referer"));
+
+        UserInfoApi userInfoApi = new UserInfoApi(apiClient);
+        UserInfo userInfo = null;
+        try {
+            userInfo = userInfoApi.getUserInfo(getIDToken(request));
+            System.out.println(userInfo);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling UserInfoApi#getUserInfo");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+
+        if (userInfo.getTenants() == null || userInfo.getTenants().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No tenants found for the user");
+        }
+
+        System.out.println("=======================planId");
+        System.out.println(planId);
+        System.out.println("=======================planIdEnd");
+        if (planId == null || planId.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No price plan found for the tenant");
+        }
+
+        System.out.println("1");
+        PricingApiClient pricingApiClient = new Configuration().getPricingApiClient();
+        System.out.println("2");
+        pricingApiClient.setReferer(request.getHeader("Referer"));
+        System.out.println("3");
+        PricingPlansApi pricingPlansApi = new PricingPlansApi(pricingApiClient);
+        System.out.println("4");
+        PricingPlan pricingPlan = null;
+        System.out.println("5");
+        try {
+            pricingPlan = pricingPlansApi.getPricingPlan(planId);
+            System.out.println("6");
+            System.out.println(pricingPlan);
+        } catch (saasus.sdk.pricing.ApiException e) {
+            System.out.println("Error");
+            System.out.println("Exception when calling PricingPlansApi#getPricingPlan");
+            System.out.println("Status code: " + e.getCode());
+            System.out.println("Reason: " + e.getResponseBody());
+            System.out.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+            throw e;
+        }
+
+        System.out.println("7");
+        return ResponseEntity.ok(pricingPlan);
     }
 }
